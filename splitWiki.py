e722b80df3b7cde462180f937035ea1e51e8dc72
James <frickjm@localhost.localdomain>
2015-07-13 16:26:45 -0400

    add splitWiki.py

---
+++ splitWiki.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 15:38:39 2015
+
+@author: frickjm
+"""
+
+storedPage  = ""
+title       = ""
+
+with open("enwiki-20150403-pages-meta-current.xml",'rb') as f:
+    for line in f:
+        if line.find("<text") > -1:
+            inContent   = True
+        if inContent:
+            storedPage  += line
+        if line.find("<title>") > -1:
+            title   = line[line.find("title")+6:line.find("</title")]
+        if line.find("</text") > -1:
+            print title
+            print storedPage
+            stop    = raw_input("")
+            inContent   = ""
+            title       = ""
+            storedPage  = ""
